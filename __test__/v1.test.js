d21ea884268c2ac5fd53cd40e25c66d77bfcb0a8
zakeyah <mom11omar@gmail.com>
2021-05-19 10:07:35 +0300

    fix the test

--- __test__/v1.test.js
+++
@@ -1,8 +0,0 @@
-"use strict"
-
-process.env.SECRET = "toes";
-
-const supergoose = require('@code-fellows/supergoose');
-const server = require('.././src/server').app;
-const mockRequest = supergoose(server);
-
